e7a0429732f50fd8f9b11fa1f505ee8636a8e33d
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-03-02 10:32:28 -0300

    oiq

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  '/',
+  function(req, res, next)
+  {
+  res.render('index', 
+    { 
+      titulo: 'Bem Vndo', 
+      mensagens: 'Vamos homens queimen a vila'
+    }
+  );
+  });
 
+
+
+/*router.get('/', nomefuncao);
+
+ function(req, res, next)
+  {
+  res.render('index', { title: 'Express' });
+  }
+);
+
+*/
 module.exports = router;
