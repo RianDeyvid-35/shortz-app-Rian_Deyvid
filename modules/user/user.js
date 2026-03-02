98a0ee9ac9a5635b2c1c6bb797e890a4e6aa3e11
RianDeyvid-35 <rian.bulhoes@gmail.com>
2026-03-02 10:31:36 -0300

    oi

--- modules/user/user.js
+++ modules/user/user.js
@@ -2,7 +2,7 @@ const { DataTypes} = require('sequelize');
 const sequelize = require('../../config/database');
 
 const User = sequelize.define(
-    'User', //nome da tabela dentro do BD MySQL
+    'User', //nome da entidade
     { //coleção de campos da tabela User
         id : {
             type: DataTypes.INTEGER,
@@ -23,7 +23,32 @@ const User = sequelize.define(
         password : {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        fullname:{
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        bio: {
+            type: DataTypes.STRING(255),
+            allowNull: true
+        },
+        profilePic :{
+            type: DataTypes.STRING(100),
+            allowNull : true,
+            defaultValue : 'default-profile.png'
+        },
+        isBlocked : {
+            type  : DataTypes.BOOLEAN,
+            defaultValue : false
+        },
+        isAdmin : {
+            type : DataTypes.BOOLEAN,
+            defaultValue : false
         }
+    },
+    {
+        tableName : 'users',
+        timestamps : true
     }
 );
 
